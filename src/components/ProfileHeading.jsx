e9a236117180942b0590b410582eea419575f731
Alexanderkoum <mbappekoum@gmail.com>
2024-06-19 08:10:34 -0400

    modified the mobile version of the project and profil heading

--- src/components/ProfileHeading.jsx
+++ src/components/ProfileHeading.jsx
@@ -4,13 +4,13 @@ import Profile from "../assets/imageprofile.png";
 
 const ProfileHeading = () => {
   return (
-    <motion.div className="flex flex-wrap-reverse md:flex-nowrap gap-7 items-center">
+    <motion.div className="flex flex-wrap-reverse md:flex-nowrap gap-7 items-center justify-center">
         
 
-        <div className="flex flex-col gap-9">
-          <p className="text-lg mb-3 font-bold py-2 mb-4 px-5 rounded-full py-2 border px-5 bg-sky-950 border-sky-900 w-full md:w-[27%]">Alexander w. koum 🤙</p>
-          <p className="text-6xl md:text-9xl mb-3 font-bold">Developpeur Front-end.</p>
-          <h1 className="text-md font-thin w-6/12">
+        <div className="flex flex-col gap-9 items-center md:items-start">
+          <p className="text-lg mb-3 font-bold py-2 mb-4 px-5 rounded-full py-2 border px-5 bg-sky-950 border-sky-900 w-auto md:w-[27%] text-center md:text-left">Alexander w. koum 🤙</p>
+          <p className="text-6xl md:text-9xl mb-3 font-bold text-center md:text-left">Developpeur Front-end.</p>
+          <h1 className="text-md font-thin w-full md:w-6/12 text-center md:text-left">
           je suis un developper front-end avec 5 ans d'experience dans la conception d'interfaces web modernes et intuitives pour une expérience utilisateur optimale à l'aide de html, css , javascript , react , wordpress et plus.
           </h1>
         </div>
