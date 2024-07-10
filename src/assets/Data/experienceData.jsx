e8ce57666f88ed13d15975458aac9a27249aecc7
Alexanderkoum <mbappekoum@gmail.com>
2024-07-10 01:27:27 -0400

    Update experienceData.jsx

--- src/assets/Data/experienceData.jsx
+++ src/assets/Data/experienceData.jsx
@@ -1,16 +1,42 @@
 const experienceData = [
     {
-        periode : '2019 - Maintenance',
-        role : 'Administrateur Salesforce - Developpeur Front End',
+        periode : 'Oct 2023 - Present',
+        role : 'Administrateur Salesforce',
         company : "Shortkut",
         jobtasks : ['Je suis un Developpeur web','Je suis avec une specialisation en front end','Je suis un Developpeur web en front end']
     },
     {
-        periode : '2017 - 2019',
-        role : 'Administrateur Salesforce - Developpeur Front End',
+        periode : 'Sept 2019 - Oct 2023',
+        role : 'Designer / Developper Web - Front end.',
+        company : "Shortkut",
+        jobtasks : ['Je suis un Developpeur web','Je suis avec une specialisation en front end','Je suis un Developpeur web en front end']
+    },
+    {
+        periode : 'Jul 2019 - Sept 2019',
+        role : 'Integrateur web - Stagiaire',
         company : "Shortkut",
         jobtasks : ['Je suis un Developpeur web','Je suis avec une specialisation en front end','Je suis un Developpeur web en front end']
     }
 ]
 
 export default experienceData;
+
+const EducationData = [
+    {
+        degree:"Certification Administrateur Salesforce",
+        school:"Salesforce Trailhead",
+        periode:"2017 - 2019",
+        location:"Montreal, Canada"
+    },
+    {
+        degree:"AEC - Design Web",
+        school:"College CDI",
+        periode:"2017 - 2019",
+        location:"Montreal, Canada"
+    },
+    {
+        degree:"Baccalaureat Bio-informatique",
+        school:"College CDI",
+        location:"Montreal, Canada"
+    }
+]
